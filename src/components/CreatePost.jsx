a09e5e3ecc9c51f08778b4edc9a1f0932550e44c
Ladyhazle2010 <sweetladyhazle@gmail.com>
2023-06-05 21:22:08 -0500

    createPost form is done

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -1,7 +1,37 @@
 import React from "react";
 
-const PostList = () => {
-  return <div className="container">Create Post Form here....</div>;
+const CreatePost = () => {
+  const onSubmit = () => {
+    console.log("form submit");
+  };
+  return (
+    <div className="container">
+      <div className="card my-3">
+        <div className="card-body">
+          <div className="card-text">
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <label>Title</label>
+                <input type="text" className="form-control" name="title" />
+              </div>
+              <div className="form-group">
+                <label>Content</label>
+                <textarea
+                  name="body"
+                  cols="30"
+                  rows="5"
+                  className="form-control"
+                ></textarea>
+              </div>
+              <button type="submit" className="btn btn-primary">
+                Create Post
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default PostList;
+export default CreatePost;
