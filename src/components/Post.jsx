8a47f81d1f61d81cec7c5569c8023eb1f2d0dd75
Ladyhazle2010 <sweetladyhazle@gmail.com>
2023-06-05 22:15:54 -0500

    like/dislike button position is changed

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,12 +5,15 @@ const Post = ({ post }) => {
     <div className="card my-3">
       <div className="card-header">
         <h5 className="text-primary">{post.title}</h5>
+        <div
+          className="d-flex"
+          style={{ position: "absolute", right: "20px", top: "10px" }}
+        >
+          <LDButton />
+          <div className="ml-5 my-auto">{post.createdAt}</div>
+        </div>
       </div>
       <div className="card-body">{post.body}</div>
-      <div className="card-footer d-flex justify-content-end">
-        <LDButton />
-        <div className="ml-5 my-auto">{post.createdAt}</div>
-      </div>
     </div>
   );
 };
